9f5c7a212fdc7b4b81380541ee727c30e539e7ca
Vincent Xu <vincentxu9@gmail.com>
2025-03-30 20:03:34 -0500

    Update description and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Sheet Music",
-  description: "Sheet music by Vincent Xu",
+  title: "Vincent's Sheet Music",
+  description: "Collection of piano sheet music by Vincent Xu",
 };
 
 export default function RootLayout({
