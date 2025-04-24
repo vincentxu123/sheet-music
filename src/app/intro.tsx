79674cae9b752d97a8e8751b25277c5a723b0b54
Vincent Xu <vincentxu9@gmail.com>
2025-04-23 21:03:53 -0500

    Small text change

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -29,7 +29,7 @@ export const Introduction = () => {
                 </div>
                 <div className="flex items-center gap-2 text-gray-600">
                   <Music className="w-4 h-4" />
-                  <span>Piano Enthusiast</span>
+                  <span>Piano Player</span>
                 </div>
               </div>
               <p className="text-gray-600 text-lg leading-relaxed">
